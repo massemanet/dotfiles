c419295e763dcf19d2d3b7c38dea37baaecae66b
mats cronqvist <masse@cronqvi.st>
2023-01-30 15:57:26 +0100

    k8sctx

--- bin/k8sctx.sh
+++ bin/k8sctx.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
-kubectl config current-context | grep -Eo '\-[a-z]*\.[a-z]*' | tr -d "-"
+_context() {
+    local CLUSTER CONTEXT
+    CLUSTER="$(kubectl config get-clusters | grep "${1:-}")" &&
+        [ "$(wc -l <<<"$CLUSTER")" == 1 ] &&
+        CONTEXT="$(kubectl config get-users | grep "$CLUSTER")" &&
+        [ "$(wc -l <<<"$CONTEXT")" == 1 ] &&
+        echo "$CONTEXT"
+}
+
+_show() {
+    kubectl config current-context | grep -Eo '\-[a-z]*\.[a-z]*' | tr -d "-"
+}
+
+_set() {
+    local CONTEXT
+    CONTEXT="$(_context "$1")"
+    if [ -z "$CONTEXT" ]
+    then echo "no context matching $1"
+    else kubectl config use-context "$CONTEXT"
+    fi
+}
+
+case "${1:-}" in
+    "") _show;;
+    *)  _set "$1";;
+esac
